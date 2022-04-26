646a3843b9ab8498b051dcb7c533dfa15b027891
Rob Johnson <rjohnson@dcs_inc.net>
2022-04-25 20:57:44 -0700

    blink onboard LED at 0.5 hz

--- distance_relay.py
+++ distance_relay.py
@@ -1,6 +1,12 @@
 import utime
+import machine
+
+led = machine.Pin(25, machine.Pin.OUT)
 
 while True:
     print('hello')
+    led.value(1)
+    utime.sleep(1.0) # 1s
+    led.value(0)
     utime.sleep(1.0) # 1s
     
