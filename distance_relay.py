dfc5a850ddb1c20614dba89346fff8705ae9c99f
Rob Johnson <rjohnson@dcs_inc.net>
2022-04-27 10:44:15 -0700

    add wdt.

--- distance_relay.py
+++ distance_relay.py
@@ -1,3 +1,4 @@
+import machine
 import utime
 import relay as relay_module  # alias so relay can be the object name
 import ultrasound
@@ -6,13 +7,21 @@ DISTANCE_THRESHOLD_CM = 50  # below this the relay should be low, above active
 
 distance_sensor = ultrasound.Ultrasound(trigger_pin=0, echo_pin=1)
 relay = relay_module.Relay(2)
+watchdog = machine.WDT(timeout=2500)  # timeout is in ms
 
 if __name__ == '__main__':
     ''' Read the range sensor once per second and update the relay based
         on whether the result is over/under DISTANCE_THRESHOLD_CM '''
+    
+    # hold the relay inactive long enough to drain residual capitance on wdt reset
+    if machine.reset_cause() == machine.WDT_RESET:
+        relay.off()
+        utime.sleep(1)
+    
     while True:
         if distance_sensor.read_cm() > DISTANCE_THRESHOLD_CM:
             relay.on()
         else:
             relay.off()
+        watchdog.feed()
         utime.sleep_us(1_000_000)  # 1s
